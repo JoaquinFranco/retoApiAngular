b7b9c39b7633ff14b656b072319c90ba6da6661d
DESKTOP-87DI9D5\joaqu <joaquinfragon@gmail.com>
2024-02-01 09:49:38 +0100

    FIX: test unitarios de utilsServices

--- src/app/services/utils.service.spec.ts
+++ src/app/services/utils.service.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
+import { Post } from '../models/post';
 import { UtilsService } from './utils.service';
 
 describe('UtilsService', () => {
@@ -12,5 +13,11 @@ describe('UtilsService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('Test createId', () => {
+    let postArr: Post[] = [{ id: 0 }];
+    let resultId = service.createId(0, postArr);
+    expect(resultId).toEqual(1);
   });
 });
